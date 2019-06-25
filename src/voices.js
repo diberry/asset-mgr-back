2c67aa41984886157d813ae4b6c16a19bae0b5ec
diberry <diberry@microsoft.com>
2019-06-24 21:47:32 -0700

    fix download path and resulting errors

---
+++ src/voices.js
@@ -0,0 +1,7 @@
+module.exports = [
+    { locale: "en-us", gender: "female", voice: "Jessa-24", code: "Jessa24KRUS" },
+    { locale: "en-us", gender: "male", voice: "Benjamin", code: "BenjaminRUS" },
+    { locale: "en-us", gender: "female", voice: "Zira", code: "ZiraRUS"},
+    { locale: "en-us", gender: "female", voice: "Jessa", code: "JessaRUS"},
+    { locale: "en-us", gender: "male", voice: "Guy", code: "Guy24kRUS" }
+];
