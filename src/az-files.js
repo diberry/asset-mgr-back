34473f88ba6439d971af5bbc909ea488822916bf
diberry <diberry@microsoft.com>
2019-07-21 08:49:31 -0700

    az files - list dirs and files - test passes

--- src/az-files.js
+++ src/az-files.js
@@ -1,8 +1,20 @@
+"use strict";
 const azure = require('azure-storage');
 
 /**
  * Share == user
  * Directory == user selects
+ * 
+ * 
+ * 
+ * https://github.com/Azure-Samples/storage-file-node-getting-started
+ * * - What is a Storage Account - http://azure.microsoft.com/en-us/documentation/articles/storage-whatis-account/
+* - Getting Started with File - https://azure.microsoft.com/en-us/documentation/articles/storage-dotnet-how-to-use-files/
+* - File Service Concepts - http://msdn.microsoft.com/en-us/library/dd179376.aspx 
+* - File Service REST API - https://msdn.microsoft.com/en-us/library/dn167006.aspx
+* - File Service Node API - http://azure.github.io/azure-storage-node/FileService.html
+* - Delegating Access with Shared Access Signatures - http://azure.microsoft.com/en-us/documentation/articles/storage-dotnet-shared-access-signature-part-1/
+*
  */
 module.exports = class AzureFiles {
         
@@ -10,9 +22,9 @@ module.exports = class AzureFiles {
 
         if(!systemConfig || !systemConfig.azstorage || !systemConfig.azstorage.connectionString || !user) throw ("az-files::Files::c'tor - missing params");
 
-        this.connectionString = systemConfig.azstorage.connectionString;
-
         this.share = user.trim().toLowerCase();
+
+        this.fileService = new azure.FileService(systemConfig.azstorage.connectionString);
     }
 
     async getFilePropertiesAsync(directory, filename){
@@ -23,9 +35,7 @@ module.exports = class AzureFiles {
     
         return new Promise(function(resolve, reject) {
     
-            const fileService = new azure.FileService(self.connectionString);
-    
-            fileService.getFileProperties(self.share, directory.toLowerCase(), filename.toLowerCase(), (error, response) => {
+            self.fileService.getFileProperties(self.share, directory.toLowerCase(), filename.toLowerCase(), (error, response) => {
                 if (error) return reject(error);
                 return resolve(response);
             });
@@ -40,12 +50,11 @@ module.exports = class AzureFiles {
     
         return new Promise(function(resolve, reject) {
     
-            const fileService = new azure.FileService(self.connectionString);
-            const primaryEndpoint = true;
-    
             var startDate = new Date();
             var expiryDate = new Date(startDate);
             expiryDate.setMinutes(startDate.getMinutes() + 5);
+            const usePrimaryEndpoint = true;
+            const shareSnapshot = undefined;
     
             const sharedAccessPolicy = {
                 AccessPolicy: {
@@ -55,9 +64,9 @@ module.exports = class AzureFiles {
                 },
               };
               
-            const sasToken = fileService.generateSharedAccessSignature(self.share, directory.toLowerCase(), filename, sharedAccessPolicy);
+            const sasToken = self.fileService.generateSharedAccessSignature(self.share, directory.toLowerCase(), filename, sharedAccessPolicy);
               
-            const url = fileService.getUrl(self.share, directory.toLowerCase(), filename, sasToken, primaryEndpoint);
+            const url = self.fileService.getUrl(self.share, directory.toLowerCase(), filename, sasToken, usePrimaryEndpoint, shareSnapshot);
     
             if(!url) reject("az-files::Files::getFileUrlAsync - url is empty");
             resolve(url);
@@ -72,15 +81,13 @@ module.exports = class AzureFiles {
     
         return new Promise(function(resolve, reject) {
     
-            const fileService = new azure.FileService(self.connectionString);
-    
-            fileService.createShareIfNotExists(self.share, error =>{
+            self.fileService.createShareIfNotExists(self.share, error =>{
                 if (error) return reject(error);
     
-                fileService.createDirectoryIfNotExists(self.share, directory.toLowerCase(), error => {
+                self.fileService.createDirectoryIfNotExists(self.share, directory.toLowerCase(), error => {
                     if (error) return reject(error);
     
-                    fileService.createFileFromLocalFile(
+                    self.fileService.createFileFromLocalFile(
                         self.share,
                         directory.toLowerCase(),
                         filename,
@@ -96,5 +103,52 @@ module.exports = class AzureFiles {
             });
         });
     }
+        
+    /**
+     * https://github.com/Azure-Samples/storage-file-node-getting-started/blob/master/fileSample.js
+     * 
+     * 
+     * Base directory denoted with empty string
+     * 
+     * 
+    */
+    async getDirectoriesAndFiles(directory){
+
+        let continuationToken = 1;
+        let items = { files: [], directories: []};  
+        let result = undefined;
+
+        const azFileOptions = null;
+
+        while(continuationToken){
+
+            result = await this.listDirectoriesAndFiles(directory, continuationToken, azFileOptions);
+
+            // files
+            items.files.push.apply(items.files, result.entries.files);
+
+            // directories
+            items.directories.push.apply(items.directories, result.entries.directories);
+
+            continuationToken = result.continuationToken;
+        }
+        return items;
+    }
+    // private to class
+    async listDirectoriesAndFiles(directory, token, options) {
+
+        let self = this;
+   
+        return new Promise(function(resolve, reject) {
     
+       
+            self.fileService.listFilesAndDirectoriesSegmented(self.share, directory, token, options, function(error, result) {
+
+                if(error) return reject(error);
+                
+                return resolve(result);
+
+            });
+        });
+    }
 }
--- test/az-files.test.js
+++ test/az-files.test.js
@@ -7,50 +7,70 @@ const string = require("../src/strings.js");
 
 describe('AzureFiles', () => {
 
-        it('should manage file', async (done) => {
+    it('should manage file', async (done) => {
 
-            try {
-                const testConfig = config.getConfigTest();
+        try {
+            jest.setTimeout(99000);
+            const testConfig = config.getConfigTest();
 
-                const fileName = `${string.dateAsTimestamp()}_short.txt`;
-                const userName = `jest-share-${string.dateAsTimestamp()}`;
-                const directory = `jest-dir-${string.dateAsTimestamp()}`;
-                const fileFullPath = path.join(testConfig.rootDir,"./data/short.txt");
+            const fileName = `${string.dateAsTimestamp()}_1_short.txt`;
+            const fileName2 = `${string.dateAsTimestamp()}_2_short.txt`;
+            const userName = `jest-share-${string.dateAsTimestamp()}`;
+            const baseDirectory = "";
+            const directory = `jest-dir-${string.dateAsTimestamp()}`;
+            const fileFullPath = path.join(testConfig.rootDir,"./data/short.txt");
 
-                const optionalContentSettings = {
-                    contentType: undefined,
-                    contentEncoding: undefined,
-                    contentLanguage: undefined
-                };
+            const optionalContentSettings = {
+                contentType: undefined,
+                contentEncoding: undefined,
+                contentLanguage: undefined
+            };
 
-                const optionalMetadata = {
-                    context: "Jest-test"
-                };
+            const optionalMetadata = {
+                context: "Jest-test"
+            };
 
-                const fileAzure = new AzureFiles(testConfig, userName);
+            const fileAzure = new AzureFiles(testConfig, userName);
 
-                // add file
-                const fileResult = await fileAzure.addFileAsync(directory, fileName, fileFullPath, optionalContentSettings, optionalMetadata);
-                expect(fileResult.lastModified).not.toEqual(undefined);
+            // add file
+            const fileResult = await fileAzure.addFileAsync(directory, fileName, fileFullPath, optionalContentSettings, optionalMetadata);
+            expect(fileResult.lastModified).not.toEqual(undefined);
 
-                // get file properties
-                const fileProperties = await fileAzure.getFilePropertiesAsync(directory, fileName);
-                expect(fileProperties.share).toEqual(userName);
+            // add file 2
+            const fileResult2 = await fileAzure.addFileAsync(directory, fileName2, fileFullPath, optionalContentSettings, optionalMetadata);
+            expect(fileResult2.lastModified).not.toEqual(undefined);
 
-                // get file download
-                const url = await fileAzure.getFileUrlAsync(directory, fileName);     
-                expect(url.indexOf("https://")).not.toEqual(-1);
-                expect(url.indexOf(userName)).not.toEqual(-1);
-                expect(url.indexOf(directory)).not.toEqual(-1);
-                expect(url.indexOf(fileName)).not.toEqual(-1);
-                    
-                done();
+            // get file properties
+            const fileProperties = await fileAzure.getFilePropertiesAsync(directory, fileName);
+            expect(fileProperties.share).toEqual(userName);
 
-            } catch(err){
-                done(err);
-            }
+            // get file download
+            const url = await fileAzure.getFileUrlAsync(directory, fileName);     
+            expect(url.indexOf("https://")).not.toEqual(-1);
+            expect(url.indexOf(userName)).not.toEqual(-1);
+            expect(url.indexOf(directory)).not.toEqual(-1);
+            expect(url.indexOf(fileName)).not.toEqual(-1);
+                
+            // get base directories
+            const directoriesAndFiles = await fileAzure.getDirectoriesAndFiles(baseDirectory);
+            expect(directoriesAndFiles).not.toBe(undefined);
+            expect(directoriesAndFiles.files.length).toBe(0);
+            expect(directoriesAndFiles.directories.length).toBe(1);
+            expect(directoriesAndFiles.directories[0].name).toEqual(directory);
 
-        });           
-    });  
+            // get subdir files
+            const subDirectoriesAndFiles = await fileAzure.getDirectoriesAndFiles(directoriesAndFiles.directories[0].name);
+            expect(subDirectoriesAndFiles).not.toBe(undefined);
+            expect(subDirectoriesAndFiles.files.length).toBe(2);
+            expect(subDirectoriesAndFiles.directories.length).toBe(0);            
+
+            done();
+
+        } catch(err){
+            done(err);
+        }
+
+    });           
+});  
 
 
