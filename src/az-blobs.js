ac28527a1fef5cbb33cd6ceeafd011090016a4ad
diberry <diberry@microsoft.com>
2019-07-25 13:31:44 -0700

    copy from file service to blob container

--- src/az-blobs.js
+++ src/az-blobs.js
@@ -1,17 +1,57 @@
 "use strict";
 const azure = require('azure-storage');
+const AzureFiles = require('./az-files.js');
 
 module.exports = class AzureBlobs {
         
     /**
      * 
-     * @param {*} systemConfig - Azure Storage connection string
+     * @param {*} systemConfig - Entire config object
      */
-    constructor(systemConfig){
+    constructor(systemConfig, appConfig){
 
         if(!systemConfig) throw ("az-blobs::c'tor - missing params");
 
+        this.appConfig = appConfig;
+        this.connectionString = systemConfig;
         this.blobService = new azure.BlobService(systemConfig);
+    }
+    /**
+     * Trim to max container length. 
+     * @param {*} containerName 
+     */
+    trimToMaxLength(containerName){
+        if(containerName.length>63){
+            return containerName.substring(0,62);
+        }
+        return containerName;
+    }
+    /**
+     * Gets properties, then constructs public URI for blob in `URI` property.
+     * @param {*} container 
+     * @param {*} blob 
+     * @param {*} options 
+     */
+    async getBlobProperties(container, blob, options=null){
+
+        if (!this.blobService || !container || !blob) throw Error("az-blobs::getBlobProperties - params missing");
+
+        let self = this;
+
+        return new Promise(function(resolve, reject) {
+
+            self.blobService.getBlobProperties(container.toLowerCase(), blob, options, (error, result) => {
+
+                if (error) return reject(error);
+
+                // adding this because it isn't returned but can be constructed
+                // appConfig.azstorage.container is the storage resource name
+                result["URI"] = `https://${self.appConfig.azstorage.container}.blob.core.windows.net/${container}/${blob}`;
+
+                return resolve(result);
+
+            });
+        });        
     }
     /**
      * 
@@ -20,16 +60,20 @@ module.exports = class AzureBlobs {
      * @param {*} originfileWithPath 
      * @param {*} options 
      * @param {*} metadata
+     * @param {*} public - boolean - defaults to false - this overrides passed in value for publicAccessLevel
      * 
      * options.publicAccessLevel:'blob'
      *
      */
-    async addBlobAsync(container, blobname, originfileWithPath, options, metadata){
+    async addBlobAsync(container, blobname, originfileWithPath, options, metadata, publicAccessToBlob=false){
 
         if (!this.blobService || !container || !blobname || !originfileWithPath) throw Error("az-blobs::addBlobAsync - params missing");
     
-        if(!options)options = {};
-        if(metadata)options.metadata = metadata;
+        if(!options){
+            options = {};
+        }
+        if(publicAccessToBlob) options.publicAccessLevel = 'blob';
+        if(metadata) options.metadata = metadata;
 
         let self = this;
 
@@ -45,27 +89,62 @@ module.exports = class AzureBlobs {
                     originfileWithPath,
                     options,
                     (error, result) => {
+
                     if (error) return reject(error);
+
                     return resolve(result);
                     }
                 );
             });
         });
     }
-    async copyFileToBlobAsync(azurreFileURI, targetContainer, targetBlob, options, metadata){
+    /**
+     * 
+     * @param {*} originatingShare - same as dir in azureFileURI
+     * @param {*} originatingFileServiceDirectory - same as dir in azureFileURI
+     * @param {*} originatingFileServiceFile - same as file in azureFileURI
+     * @param {*} originatingFileServiceAccessPermissions - default to readonly for 5 min
+     * @param {*} azureFileURI 
+     * @param {*} targetContainer - same as dir in azureFileURI, will be prepended with `pub-`
+     * @param {*} targetBlob - same as file in azureFileURI
+     * @param {*} options - defaults for copy to blob only
+     * @param {*} metadata - metadata for blob only
+     */
+    async copyFileToBlobAsync(originatingShare, originatingFileServiceDirectory, originatingFileServiceFile, originatingFileServiceAccessPermissions=null, azureFileURI,  targetContainer, targetBlob, options, metadata){
 
-        if (!storageConnectionString || !azurreFileURI || !targetContainer || !targetBlob || !originfileWithPath) throw Error("az-blobs::copyFileToBlobAsync - params missing");
+        if (!this.appConfig || !this.blobService || !originatingShare || !originatingFileServiceDirectory || !originatingFileServiceFile || !azureFileURI || !targetContainer || !targetBlob ) throw Error("az-blobs::copyFileToBlobAsync - params missing");
     
         if(!options)options = {};
         if(metadata)options.metadata = metadata;
 
+        let self = this;
+
+        // Assumes File Share is exactly the same as Blob Container
+        const azureFiles = new AzureFiles(self.appConfig, originatingShare);
+        const accessToken = azureFiles.getAccessToken(originatingFileServiceDirectory, originatingFileServiceFile);
+
+        if(!accessToken)throw "az-blobs::copyFileToBlobAsync - can't get access token";
+
+        azureFileURI = `${azureFileURI}?${accessToken}`;
+
         return new Promise(function(resolve, reject) {
     
-            const blobService = azure.createBlobService(storageConnectionString);
+            // blob will be publicly accessible
+            // container will not return a public listing
+            const containerOptions = {
+                publicAccessLevel: 'blob'
+            };
 
-            blobService.startCopyBlob(azureFileUri, targetContainer.toLowerCase(), targetBlob, options, (error, result) => {
+            self.blobService.createContainerIfNotExists(targetContainer, containerOptions, error => {
+
                 if (error) return reject(error);
-                return resolve(result);
+
+                self.blobService.startCopyBlob(azureFileURI, targetContainer, targetBlob, options, (error, result) => {
+
+                    if (error) return reject(error);
+
+                    return resolve(result);
+                });
             });
         });
     }
--- test/az-blobs.test.js
+++ test/az-blobs.test.js
@@ -5,7 +5,7 @@ const string = require("../src/strings.js");
 
 describe('AzureBlobs', () => {
 
-    it.only('should add to blob container', async (done) => {
+    it('should add to blob container as private file', async (done) => {
 
         try {
             const testConfig = config.getConfigTest();
@@ -13,11 +13,13 @@ describe('AzureBlobs', () => {
             const fileName = `short.txt`;
             const fileFullPath = path.join(testConfig.rootDir,"./data/" + fileName);
             const options = {};
+            const timestamp = string.dateAsTimestamp();
 
-            const containername = `${string.dateAsTimestamp()}${testConfig.azstorage.container}`;
-            const blobname = `${string.dateAsTimestamp()}_${fileName}`;
+            const blobname = `${fileName}`;
 
             const blobService = new AzureBlobs(testConfig.azstorage.connectionString);
+
+            const containername = blobService.trimToMaxLength(`test-ab-067d3c58-1332-4db5-8ed4-7a935fc91002-${timestamp}`);
 
             const blobResult = await blobService.addBlobAsync(containername, blobname, fileFullPath, options);
 
@@ -30,26 +32,72 @@ describe('AzureBlobs', () => {
 
     });
 
-    it('should add to blob container', async (done) => {
+    it('should add to blob container as public file', async (done) => {
+
+        try {
+            const testConfig = config.getConfigTest();
+
+            const fileName = `short.txt`;
+            const fileFullPath = path.join(testConfig.rootDir,"./data/" + fileName);
+            const containerOptions = {
+                publicAccessLevel: 'blob'
+            };
+            const timestamp = string.dateAsTimestamp();
+
+            const blobname = `${fileName}`;
+
+            const blobService = new AzureBlobs(testConfig.azstorage.connectionString);
+
+            const containername = blobService.trimToMaxLength(`test-ab-067d3c58-1332-4db5-8ed4-7a935fc91002-${timestamp}`);
+
+            const blobResult = await blobService.addBlobAsync(containername, blobname, fileFullPath, containerOptions);
+
+            expect(blobResult.lastModified).not.toEqual(undefined);
+            done();
+
+        } catch(err){
+            done(err);
+        }
+
+    });    
+
+    // TBD: get access token
+    it.only('should copy to blob container as public file', async (done) => {
 
         try {
             const testConfig = config.getConfigTest();
 
             // using container and file from user1
+            const targetFileName = `short_en.mp3`;
+            const sourceURI = `https://diberryassetmgrtest.file.core.windows.net/067d3c58-1332-4db5-8ed4-7a935fc91002/this-is-a-test/short_en.mp3`;
 
-            const fileName = `short_en.mp3`;
-            const sourceURI = "https://diberryassetmgrtest.file.core.windows.net/067d3c58-1332-4db5-8ed4-7a935fc91002/this-is-a-test/short_en.mp3";
 
-            const targetContainer = "067d3c58-1332-4db5-8ed4-7a935fc91002";
+            const timestamp = string.dateAsTimestamp();
+            const blobOptions = undefined;
+            const blobMetadata = undefined;
 
-            const options = undefined;
-            const metadata = undefined;
+            // for accessToken
+            const originatingShare = "067d3c58-1332-4db5-8ed4-7a935fc91002";
+            const originatingDirectory = "this-is-a-test";
+            const originatingFile = "short_en.mp3"
+            const originatingPermissions = null;
 
-            const blobService = new Blob(testConfig.azstorage.connectionString);
+            const blobService = new AzureBlobs(testConfig.azstorage.connectionString, testConfig);
 
-            const blobResult = await blobService.addBlobAsync(containername, blobname, fileFullPath, options, metadata);
+            const targetContainer = blobService.trimToMaxLength(`test-cb-${timestamp}`);
+
+            const blobResult = await blobService.copyFileToBlobAsync(
+                originatingShare, originatingDirectory, originatingFile, originatingPermissions, sourceURI, targetContainer, targetFileName, blobOptions, blobMetadata);
 
             expect(blobResult.lastModified).not.toEqual(undefined);
+
+            const optionsForProperties = undefined;
+
+            const blobProperties = await blobService.getBlobProperties(targetContainer, targetFileName, optionsForProperties);
+
+            expect(blobProperties.lastModified).not.toEqual(undefined);
+            expect(blobProperties.URI).not.toEqual(undefined);
+
             done();
 
         } catch(err){
